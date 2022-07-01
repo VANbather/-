37d839758e8b0ceba4545996c32c9ca5269ad681
VANbather <108210915+VANbather@users.noreply.github.com>
2022-07-02 05:38:09 +0800

    Add files via upload

---
+++ 22T2-Web d-Design-VAN_Assignment 03-self introduction -games/js/js.js
@@ -0,0 +1,13 @@
+<JS>
+    <script src="js/modernizr-latest.js"></script>
+    <script src="js/jquery-1.8.2.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="js/jquery.isotope.min.js" type="text/javascript"></script>
+    <script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>
+    <script src="js/jquery.nav.js" type="text/javascript"></script>
+    <script src="js/jquery.cslider.js" type="text/javascript"></script>
+ <script src="contact/jqBootstrapValidation.js"></script>
+ <script src="contact/contact_me.js"></script>
+
+    <script src="js/custom.js" type="text/javascript"></script>
+    <script src="js/owl-carousel/owl.carousel.js"></script>
